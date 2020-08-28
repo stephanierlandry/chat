5dc4a0728f6c443ea6eee441c90f2f4f1e30101e
Stephanierlandry <stephanierlandry@gmail.com>
2020-08-28 11:21:08 -0500

    Background color buttons created and styled

--- hello-world/components/Start.js
+++ hello-world/components/Start.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
 import  { Button } from 'react-native-elements';
 
 export default class Screen1 extends React.Component {
@@ -19,21 +19,32 @@ export default class Screen1 extends React.Component {
           <View style={styles.titleContainer}>
             <Text style={styles.title}>Hello World</Text>
           </View>
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.input}
-              onChangeText={(name) => this.setState({name})}
-              value={this.state}
-              placeholder="Your Name"
-              placeholderTextColor='#757083'
-            />
-          </View>
-          <View style={styles.buttonContainer}>
-            <Button
-              title="Start Chatting"
-              onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name })}
-              buttonStyle={{backgroundColor: '#757083', width:320, height:60, justifyContent: 'center', borderRadius:2}}
-            />
+          <View style={styles.container}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                onChangeText={(name) => this.setState({name})}
+                value={this.state}
+                placeholder="Your Name"
+                placeholderTextColor='#757083'
+              />
+            </View>
+            <View style={styles.backgroundColorContainer}>
+              <Text style={styles.backgroundColorText}>{'Choose Background Color:'}</Text>
+              <View style={styles.colorButtonContainer}>
+                <TouchableOpacity style={[styles.colorButton, styles.color1]}></TouchableOpacity>
+                <TouchableOpacity style={[styles.colorButton, styles.color2]}></TouchableOpacity>
+                <TouchableOpacity style={[styles.colorButton, styles.color3]}></TouchableOpacity>
+                <TouchableOpacity style={[styles.colorButton, styles.color4]}></TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Start Chatting"
+                onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name })}
+                buttonStyle={{backgroundColor: '#757083', width:320, height:60, justifyContent: 'center', borderRadius:2}}
+              />
+            </View>
           </View>
         </ImageBackground>
 
@@ -57,15 +68,19 @@ const styles = StyleSheet.create({
     color:'#FFFFFF',
     fontSize: 45,
     fontWeight:'600'
-
+  },
+  container: {
+    backgroundColor: 'white',
+    height: '44%',
+    width: '88%',
+    flex:1,
+    marginBottom: 25
   },
   inputContainer:{
     backgroundColor: 'white',
-    alignItems: 'center',
+    alignSelf: 'center',
     justifyContent: 'flex-start',
-    height: '44%',
     width: '88%',
-    marginBottom: 27,
     flex:1
   },
   input: {
@@ -73,16 +88,54 @@ const styles = StyleSheet.create({
     borderColor: '#757083',
     borderWidth: 2,
     borderRadius:3,
-    width: '88%',
+    width: '100%',
     fontSize: 16,
     marginTop: 25,
     paddingLeft:7,
     opacity: 0.5,
     fontWeight: '300'
   },
+  backgroundColorContainer:{
+    flex:1,
+    justifyContent:'center',
+    textAlign:'left',
+    marginTop: 25
+  },
+  backgroundColorText:{
+    fontSize:16,
+    color: '#757083',
+    fontWeight:'600',
+    paddingLeft:22
+  },
+  colorButtonContainer:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    alignItems:'center',
+    marginStart:20
+  },
+  colorButton:{
+    borderRadius:50,
+    width:40,
+    height:40,
+    marginHorizontal:10
+  },
+  color1:{
+    backgroundColor: '#090C08'
+  },
+  color2:{
+    backgroundColor: '#474056'
+  },
+  color3:{
+    backgroundColor: '#8A95A5'
+  },
+  color4:{
+    backgroundColor:'#B9C6AE'
+  },
   buttonContainer:{
     flex:1,
     justifyContent:'flex-end',
-    marginBottom: 25
+    marginBottom: 25,
+    alignSelf:'center'
   }
 });
