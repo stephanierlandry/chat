1e55f34d1fa500fe70b92e9e0d789ba8eaa46087
Stephanierlandry <stephanierlandry@gmail.com>
2020-08-27 20:50:17 -0500

    layout fixed on start page

--- hello-world/components/Start.js
+++ hello-world/components/Start.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
 
 export default class Screen1 extends React.Component {
 
@@ -11,19 +11,47 @@ export default class Screen1 extends React.Component {
 
   render() {
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello World</Text>
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 180}}
-          onChangeText={(name) => this.setState({name})}
-          value={this.state.name}
-          placeholder='Type here ...'
-        />
-        <Button
-          title="Start Chatting"
-          onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name })}
-        />
-      </View>
-    )
+
+        <ImageBackground style={styles.backgroundImage} source={require('../assets/BackgroundImage.png')}>
+          <View style={styles.inputContainer}>
+            <Text>Hello World</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(name) => this.setState({name})}
+              value={this.state.name}
+              placeholder='Type here ...'
+            />
+            <Button
+              title="Start Chatting"
+              onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name })}
+            />
+            </View>
+        </ImageBackground>
+
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 180
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    justifyContent:'flex-end',
+    alignItems: 'center'
+  },
+  inputContainer:{
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '44%',
+    width: '88%',
+    marginBottom: 27
+  }
+});
