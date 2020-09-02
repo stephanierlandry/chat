d5e751bbd21e0561112297ec9dc40f60d258171a
Stephanierlandry <stephanierlandry@gmail.com>
2020-09-01 20:16:41 -0500

    Chats being sent and looking like average chat screen

--- hello-world/components/Chat.js
+++ hello-world/components/Chat.js
@@ -1,16 +1,68 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, InputToolbar } from 'react-native-gifted-chat'
 
 export default class Chat extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+  /*messages follows Gifted Chat's format*/
+  componentDidMount() {
+   this.setState({
+     messages: [
+       {
+         _id: 1,
+         text: 'Hello developer',
+         createdAt: new Date(),
+         user: {
+           _id: 2,
+           name: 'React Native',
+           avatar: 'https://placeimg.com/140/140/any',
+         },
+       },
+     ],
+   })
+  }
+
+  /* the message a user has just sent gets appended to the state messages so that it can be displayed in the chat*/
+  onSend(messages = []) {
+   this.setState(previousState => ({
+     messages: GiftedChat.append(previousState.messages, messages),
+   }))
+  }
 
   render() {
     let name = this.props.route.params.name;
 
     this.props.navigation.setOptions({ title: name });
 
+    // const customtInputToolbar = props => {
+    //   return (
+    //     <InputToolbar
+    //       {...props}
+    //       containerStyle={{
+    //         borderTopWidth: 1,
+    //         padding: 1
+    //       }}
+    //     />
+    //   );
+    // };
+
     return (
-      <View style={{backgroundColor:this.props.route.params.color, flex: 1, alignItems:'center'}}>
-        <Text style={styles.text}> hello {name}</Text>
+      <View style={{backgroundColor:this.props.route.params.color, flex: 1}}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     );
   };
@@ -18,12 +70,5 @@ export default class Chat extends React.Component {
 
 const styles = StyleSheet.create({
 
-  text:{
-    color:'white',
-    flex:1,
-    justifyContent:'center',
-    alignItems: 'center',
-    margin:50,
-    fontSize: 24
-  }
+
 })
