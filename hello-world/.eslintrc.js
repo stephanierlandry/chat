fd73be4117be793c739d1b5c8ad228755ca475bb
Stephanierlandry <stephanierlandry@gmail.com>
2020-09-22 13:15:27 -0500

    eslint installed and configured. babel-eslint installed

---
+++ hello-world/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    'react-native/react-native': true
+  },
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-native'
+  ],
+  rules: {
+  },
+};
